4fe05be859d50f43b3f75a3254d9ce8b69eaf8b4
Hồ Minh Trí <hominhtri96hcmuit@gmail.com>
2017-12-30 23:05:27 +0700

    Cut Template Admin Master, Product

--- resources/views/admin/product/add.blade.php
+++ resources/views/admin/product/add.blade.php
@@ -1,218 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Khóa Học Lập Trình Laravel Framework 5.x Tại Khoa Phạm">
-    <meta name="author" content="">
-    <title>Admin</title>
-    <base href="{{asset('')}}">
-    <!-- Bootstrap Core CSS -->
-    <link href="admin/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="admin/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="admin/dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="admin/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- DataTables CSS -->
-    <link href="admin/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
-
-    <!-- DataTables Responsive CSS -->
-    <link href="admin/bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
-    <script src="{{url('templateEditor/ckeditor/ckeditor.js')}}"></script>
-
-      <script type="text/javascript">
-          var baseURL = "{!! url('/') !!}";
-
-      </script>
-</head>
-
-<body>
-
-    <div id="wrapper">
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">Admin Area</a>
-            </div>
-            <!-- /.navbar-header -->
-
-            <ul class="nav navbar-top-links navbar-right">
-                <!-- /.dropdown -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-default" type="button">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Category<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">List Category</a>
-                                </li>
-                                <li>
-                                    <a href="#">Add Category</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-cube fa-fw"></i> Product<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">List Product</a>
-                                </li>
-                                <li>
-                                    <a href="#">Add Product</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-users fa-fw"></i> User<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">List User</a>
-                                </li>
-                                <li>
-                                    <a href="#">Add User</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                    </ul>
+@extends('admin.master')
+@section('content')
+    <!-- Page Content -->
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Product
+                        <small>Add</small>
+                    </h1>
                 </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
+                <!-- /.col-lg-12 -->
+                <div class="col-lg-7" style="padding-bottom:120px">
 
-        <!-- Page Content -->
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">Product
-                            <small>Add</small>
-                        </h1>
-                    </div>
-                    <!-- /.col-lg-12 -->
-                    <div class="col-lg-7" style="padding-bottom:120px">
+                    <form action="{{route('admin.product.getAdd')}}" method="POST">
+                        <input type="hidden" name="_token" value="{{csrf_token()}}">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input class="form-control" name="txtName" placeholder="Please Enter Username" required/>
+                        </div>
 
-                        <form action="{{route('admin.product.getAdd')}}" method="POST">
-                            <input type="hidden" name="_token" value="{{csrf_token()}}">
-                            <div class="form-group">
-                                <label>Name</label>
-                                <input class="form-control" name="txtName" placeholder="Please Enter Username" required/>
-                            </div>
+                        <div class="form-group">
+                            <label>Category</label>
+                            <input class="form-control" name="txtCategory" placeholder="Please Enter Password" required/>
+                        </div>
 
-                            <div class="form-group">
-                                <label>Category</label>
-                                <input class="form-control" name="txtCategory" placeholder="Please Enter Password" required/>
-                            </div>
+                        <div class="form-group">
+                            <label>Description</label>
+                            <textarea class="form-control" rows="txtDescription" name="txtIntro"></textarea>
+                        </div>
+                        <script type="text/javascript">CKEDITOR.replace( 'txtIntro' );</script>
 
-                            <div class="form-group">
-                                <label>Description</label>
-                                <textarea class="form-control" rows="txtDescription" name="txtIntro"></textarea>
-                            </div>
-                            <script type="text/javascript">CKEDITOR.replace( 'txtIntro' );</script>
+                        <div class="form-group">
+                            <label>Price</label>
+                            <input class="form-control" name="txtPrice" placeholder="Please Enter Username" />
+                        </div>
 
-                            <div class="form-group">
-                                <label>Price</label>
-                                <input class="form-control" name="txtPrice" placeholder="Please Enter Username" />
-                            </div>
+                        <div class="form-group">
+                            <label>Promotion</label>
+                            <input class="form-control" name="txtPromotion" placeholder="Please Enter Username" />
+                        </div>
 
-                            <div class="form-group">
-                                <label>Promotion</label>
-                                <input class="form-control" name="txtPromotion" placeholder="Please Enter Username" />
-                            </div>
-
-                            <div class="form-group">
-                                <label>Images</label>
-                                <input type="file" name="fImages">
-                            </div>
-                            <button type="submit" class="btn btn-default">Product Add</button>
-                            <button type="reset" class="btn btn-default">Cancel</button>
-                        <form>
-                    </div>
+                        <div class="form-group">
+                            <label>Images</label>
+                            <input type="file" name="fImages">
+                        </div>
+                        <button type="submit" class="btn btn-default">Product Add</button>
+                        <button type="reset" class="btn btn-default">Cancel</button>
+                    <form>
                 </div>
-                <!-- /.row -->
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
         </div>
-        <!-- /#page-wrapper -->
-
+        <!-- /.container-fluid -->
     </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="admin/bower_components/jquery/dist/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="admin/bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="admin/dist/js/sb-admin-2.js"></script>
-
-    <!-- DataTables JavaScript -->
-    <script src="admin/bower_components/DataTables/media/js/jquery.dataTables.min.js"></script>
-    <script src="admin/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
-
-    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
-    <script>
-    $(document).ready(function() {
-        $('#dataTables-example').DataTable({
-                responsive: true
-        });
-    });
-    </script>
-</body>
-
-</html>
+    <!-- /#page-wrapper -->
+@endsection
